87e7816b4bd074f41507459d4fed8d299e097e98
jay <yjzhway@126.com>
2021-07-05 12:53:41 +0800

    初始化仓库

---
+++ TestHPSocket/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace TestHPSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(700, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 83);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(630, 355);
+            this.txtLog.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(687, 83);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(630, 355);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(607, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(370, 14);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(100, 21);
+            this.txtContent.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(793, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "断开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(887, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "停止状态查询";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1453, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ TestHPSocket/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HPSocket.Tcp;
+using HPSocket;
+using Newtonsoft.Json;
+
+namespace TestHPSocket
+{
+    public partial class Form1 : Form
+    {
+        readonly ITcpPullAgent _agent = new TcpPullAgent();
+        private const int MaxPacketSize = 4096;
+        delegate void AddLogHandler(string log);
+        IntPtr[] connIds = new IntPtr[2];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AddLog($"connId = {connIds[0]}  connectState = {_agent.GetConnectionState(connIds[0])}");
+            AddLog($"connId = {connIds[1]}  connectState = {_agent.GetConnectionState(connIds[1])}");
+
+        }
+
+        private void AddLog(string log)
+        {
+            if (txtLog.IsDisposed)
+            {
+                return;
+            }
+
+            // 从ui线程去操作ui
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new AddLogHandler(AddLog), log);
+            }
+            else
+            {
+                txtLog.AppendText($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {log}\r\n");
+            }
+        }
+
+        private void AddLog1(string log)
+        {
+            if (textBox1.IsDisposed)
+            {
+                return;
+            }
+
+            // 从ui线程去操作ui
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new AddLogHandler(AddLog1), log);
+            }
+            else
+            {
+                textBox1.AppendText($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {log}\r\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 缓冲区大小
+            _agent.SocketBufferSize = 4096; // 4K
+            // 异步连接
+            _agent.Async = true;
+            // 异步连接可以设置连接超时时间, 单位是毫秒
+            _agent.ConnectionTimeout = 3000;
+
+            // 注意这里是监听地址, 连接服务器的ip和端口在调用Connect()的时候传入
+            _agent.Address = "0.0.0.0";
+
+            _agent.OnPrepareConnect += _agent_OnPrepareConnect;
+            _agent.OnConnect += _agent_OnConnect;
+            _agent.OnReceive += _agent_OnReceive;
+            _agent.OnClose += _agent_OnClose;
+        }
+
+        private HandleResult _agent_OnClose(IAgent sender, IntPtr connId, SocketOperation socketOperation, int errorCode)
+        {
+            AddLog1($"OnClose({connId}), socket operation: {socketOperation}, error code: {errorCode}");
+            return HandleResult.Ok;
+        }
+
+        private HandleResult _agent_OnReceive(IAgent sender, IntPtr connId, int length)
+        {
+            if (!(sender is ITcpPullAgent agent))
+            {
+                return HandleResult.Error;
+            }
+
+            // 封包头长度
+            const int headerLength = sizeof(int);
+            HandleResult result;
+            do
+            {
+                // 窥探缓冲区, 取头部字节得到包头
+                var fr = agent.Peek(connId, headerLength, out var packetHeader);
+
+                // 连接已断开
+                if (fr == FetchResult.DataNotFound)
+                {
+                    result = HandleResult.Error;
+                    break;
+                }
+
+                // 如果来的数据长度不够封包头的长度, 等下次在处理 
+                if (fr == FetchResult.LengthTooLong)
+                {
+                    result = HandleResult.Ignore;
+                    break;
+                }
+
+                // 两端字节序要保持一致
+                // 如果当前环境不是小端字节序
+                if (!BitConverter.IsLittleEndian)
+                {
+                    // 翻转字节数组, 变为小端字节序
+                    Array.Reverse(packetHeader);
+                }
+
+                // 得到包头指向的数据长度
+                var dataLength = BitConverter.ToInt32(packetHeader, 0);
+
+                // 完整的包长度(含包头和完整数据的大小)
+                var fullLength = dataLength + headerLength;
+
+                if (dataLength < 0 || fullLength > MaxPacketSize)
+                {
+                    result = HandleResult.Error;
+                    break;
+                }
+
+                // 如果缓冲区数据长度不够一个完整的包, 当前不处理
+                if (length < fullLength)
+                {
+                    result = HandleResult.Ignore;
+                    break;
+                }
+
+                // 从缓冲区取数据, 注意取的是 fullLength 长的包
+                fr = agent.Fetch(connId, fullLength, out var data);
+
+                // 连接已断开
+                if (fr == FetchResult.DataNotFound)
+                {
+                    result = HandleResult.Error;
+                    break;
+                }
+
+                // 如果来的数据长度不够封包头的长度, 等下次在处理 
+                if (fr == FetchResult.LengthTooLong)
+                {
+                    result = HandleResult.Ignore;
+                    break;
+                }
+
+                // 逻辑上fr到这里必然ok
+
+                // 注意: 现在data里是包含包头的
+                // 到这里当前只处理这一个包, 其他的数据不fetch, 等下次OnReceive到达处理
+                result = OnProcessFullPacket(sender, connId, data, headerLength);
+                if (result == HandleResult.Error)
+                {
+                    break;
+                }
+
+                // 继续下一次循环
+
+            } while (true);
+
+            return result;
+        }
+
+        private HandleResult OnProcessFullPacket(IAgent sender, IntPtr connId, byte[] data, int headerLength)
+        {
+            // 这里来的都是完整的包
+            // 但是因为数据是包含包头的, 所以转字符串的时候注意 Encoding.UTF8.GetString() 的用法
+            var packet = JsonConvert.DeserializeObject<Packet>(Encoding.UTF8.GetString(data, headerLength, data.Length - headerLength));
+            var result = HandleResult.Ok;
+            switch (packet.Type)
+            {
+                case PacketType.Echo: // 回显是个字符串显示操作
+                    {
+                        AddLog1($"OnProcessFullPacket(), type: {packet.Type}, content: {packet.Data}");
+                        break;
+                    }
+                case PacketType.Time: // 获取服务器时间依然是个字符串操作^_^
+                    {
+                        AddLog1($"OnProcessFullPacket(), type: {packet.Type}, time: {packet.Data}");
+                        break;
+                    }
+                default:
+                    result = HandleResult.Error;
+                    break;
+            }
+            return result;
+        }
+
+        private HandleResult _agent_OnConnect(IAgent sender, IntPtr connId, IProxy proxy)
+        {
+            return HandleResult.Ok;
+        }
+
+        private HandleResult _agent_OnPrepareConnect(IAgent sender, IntPtr connId, IntPtr socket)
+        {
+            return HandleResult.Ok;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_agent.HasStarted)
+            {
+                MessageBox.Show(@"请先断开与服务器的连接", @"正在通信:", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                e.Cancel = true;
+                return;
+            }
+
+            // 停止并释放客户端
+            _agent.Dispose();
+
+            e.Cancel = false;
+        }
+
+
+        /// <summary>
+        /// 发送包头
+        /// <para>固定包头网络字节序</para>
+        /// </summary>
+        /// <param name="sender">服务器组件</param>
+        /// <param name="connId">连接id</param>
+        /// <param name="dataLength">实际数据长度</param>
+        /// <returns></returns>
+        private bool SendPacketHeader(IAgent sender, IntPtr connId, int dataLength)
+        {
+            // 包头转字节数组
+            var packetHeaderBytes = BitConverter.GetBytes(dataLength);
+
+            // 两端字节序要保持一致
+            // 如果当前环境不是小端字节序
+            if (!BitConverter.IsLittleEndian)
+            {
+                // 翻转字节数组, 变为小端字节序
+                Array.Reverse(packetHeaderBytes);
+            }
+
+            return sender.Send(connId, packetHeaderBytes, packetHeaderBytes.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_agent.Start())
+            {
+                throw new Exception($"Start() error code: {_agent.ErrorCode}, error message: {_agent.ErrorMessage}");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                if (!_agent.Connect("127.0.0.1", 5555, out connIds[i]))
+                {
+                    throw new Exception($"error code: {_agent.ErrorCode}, error message: {_agent.ErrorMessage}");
+                }
+            }
+            
+            timer1.Enabled = true;
+        }
+
+
+        /// <summary>
+        /// 封包
+        /// </summary>
+        public class Packet
+        {
+            /// <summary>
+            /// 封包类型
+            /// </summary>
+            public PacketType Type { get; set; }
+
+            /// <summary>
+            /// 数据
+            /// </summary>
+            public string Data { get; set; }
+        }
+
+        /// <summary>
+        /// 封包类型
+        /// </summary>
+        public enum PacketType
+        {
+            /// <summary>
+            /// 回显
+            /// </summary>
+            Echo = 1,
+            /// <summary>
+            /// 时间
+            /// </summary>
+            Time
+        }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="connId"></param>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        private void Send(IAgent sender, IntPtr connId, PacketType type, string data)
+        {
+            if (!_agent.HasStarted)
+            {
+                return;
+            }
+
+            // 组织封包, 取得要发送的数据
+            var packet = new Packet
+            {
+                Type = type,
+                Data = data,
+            };
+
+            var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(packet));
+
+            // 先发包头
+            if (!SendPacketHeader(sender, connId, bytes.Length))
+            {
+                _agent.Disconnect(connId);
+                return;
+            }
+
+            // 再发实际数据
+            if (!_agent.Send(connId, bytes, bytes.Length))
+            {
+                _agent.Disconnect(connId);
+            }
+        }
+
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            await _agent.StopAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var connIds = _agent.GetAllConnectionIds();
+            foreach (var connId in connIds)
+            {
+                Send(_agent, connId, PacketType.Echo, txtContent.Text.Trim());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text.Equals("停止状态查询"))
+            {
+                timer1.Enabled = false;
+                button4.Text = "启动状态查询";
+            }
+            else if (button4.Text.Equals("启动状态查询"))
+            {
+                timer1.Enabled = true;
+                button4.Text = "停止状态查询";
+            }
+        }
+    }
+}
